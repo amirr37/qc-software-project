dddc12655288c571d16149d4a9bc7c73251ac666
amir jahangiri <amirrj037@gmail.com>
2023-07-04 16:25:32 +0330

    create home_module and add it to intalled apps

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 
     # internal apps
     'account_module',
+    'home_module',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# todo: fix login url
