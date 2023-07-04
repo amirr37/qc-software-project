6c62fe44fddfe1d743195c060ee479f8c3262a11
amir jahangiri <amirrj037@gmail.com>
2023-07-04 16:18:57 +0330

    create login view and template and fix it url

--- account_module/views.py
+++ account_module/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import LoginForm
 
 
 # Create your views here.
 
+
+class LoginView(View):
+    def get(self, request):
+        context = {'login_form': LoginForm()}
+        return render(request, 'account_module/login.html', context)
+
+    def post(self, request):
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                # return redirect('index_page')
+                # todo : create index_page view
+            else:
+                login_form.add_error('password', 'Invalid username or password.')
+
+        context = {'login_form': login_form}
+        return render(request, 'account_module/login.html', context)
