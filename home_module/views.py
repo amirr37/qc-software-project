32b8ae51f5fcd008f001cce8cd7bb22b8ef89735
amir jahangiri <amirrj037@gmail.com>
2023-07-04 17:02:27 +0330

    make index page required to login and set LOGIN_URL in settings

--- home_module/views.py
+++ home_module/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
 
 
-class IndexPageView(TemplateView):
+class IndexPageView(LoginRequiredMixin, TemplateView):
     template_name = 'home_module/index.html'
