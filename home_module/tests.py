7d7171f3fa8575e19b9dd60340c9dbf324bced81
amir jahangiri <amirrj037@gmail.com>
2023-07-14 18:16:43 +0330

    create indexView test

--- home_module/tests.py
+++ home_module/tests.py
@@ -1,11 +1,27 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.urls import reverse
+
 
 # Create your tests here.
 
 
 class IndexViewTest(TestCase):
-    def setUp(self) :
-        pass
+    def setUp(self):
+        self.client = Client()
+        self.login_url = reverse('login')
+        self.index_url = reverse('index_page')
+        self.username = 'testuser'
+        self.password = 'testpassword'
+        self.email = 'test@test.com'
+        self.user = get_user_model().objects.create_user(username=self.username, password=self.password,
+                                                         email=self.email)
 
     def test_index_view(self):
-        pass
+        # Log in the user
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.get(self.index_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home_module/index.html')
+        self.assertTrue(response.context['user'].is_authenticated)
